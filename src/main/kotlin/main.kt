c876b8d60d8c32508c2950dcaa2ba1b4a4ac8bde
TsuraEvgesha <primak.evgesha@mail.ru>
2022-04-11 15:52:45 +0700

    Задание модернизировано

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,20 @@
 fun main() {
-    val amount: Int = 500000
+    println(comissionTransfer())
+    }
+fun comissionTransfer(){
+    val sumTransfer = 100000
     val procent = 100
-    val commission: Double = (amount/procent)*0.75
+    val commisVisaMir: Double = (sumTransfer/procent)*0.75
     val min = 350
-    if(commission<min) println("Cумма комиссии: ${min} копеек") else println("Сумма комиссии: ${commission.toInt()} копеек")
+    val transfersUpto=0
+    val card= "Vk Pay"
+    when(true){
+        card=="Vk Pay" -> println("Перевод без комиссии")
+        card=="MasterCard"|| card=="Maestro" -> if (transfersUpto<750000)
+            println("Перевод без комиссии") else println("Сумма комиссии ${(sumTransfer/procent)*0.6}")
+        card=="Visa" || card == "Мир" -> if(commisVisaMir<min)
+            println("Cумма комиссии: ${min} копеек") else println("Сумма комиссии: ${commisVisaMir.toInt()} копеек")
+}
+
 
 }
