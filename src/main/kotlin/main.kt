31d6e240c49383954c11bc30380fdb3ba3de8ed4
TsuraEvgesha <primak.evgesha@mail.ru>
2022-04-13 13:24:52 +0700

    Изменение проекта

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,14 +4,16 @@ fun main() {
 fun comissionTransfer(){
     val sumTransfer = 100000
     val procent = 100
-    val commisVisaMir: Double = (sumTransfer/procent)*0.75
+    val discount = 0.75
+    val discount2= 0.6
+    val commisVisaMir: Double = (sumTransfer/procent)*discount
     val min = 350
     val transfersUpto=0
     val card= "Vk Pay"
     when(true){
         card=="Vk Pay" -> println("Перевод без комиссии")
         card=="MasterCard"|| card=="Maestro" -> if (transfersUpto<750000)
-            println("Перевод без комиссии") else println("Сумма комиссии ${(sumTransfer/procent)*0.6}")
+            println("Перевод без комиссии") else println("Сумма комиссии ${(sumTransfer/procent)*discount2}")
         card=="Visa" || card == "Мир" -> if(commisVisaMir<min)
             println("Cумма комиссии: ${min} копеек") else println("Сумма комиссии: ${commisVisaMir.toInt()} копеек")
 }
